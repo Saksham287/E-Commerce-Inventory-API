6f520401dca135715cc5363732f11b4c42a88672
Saksham Gupta <saksham@sakshams-air.mynetworksettings.com>
2026-05-01 04:03:29 -0400

    Day 2

---
+++ init_db.py
@@ -0,0 +1,56 @@
+import pymysql
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+conn = pymysql.connect(
+    host=os.getenv("DB_HOST"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME")
+)
+
+cursor = conn.cursor()
+
+# Create database
+cursor.execute("CREATE DATABASE IF NOT EXISTS ecommerce_inventory;")
+cursor.execute("USE ecommerce_inventory;")
+
+# Create users table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    role ENUM('Admin', 'Staff') NOT NULL
+);
+""")
+
+# Create categories table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    description TEXT
+);
+""")
+
+# Create products table
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    sku VARCHAR(50) UNIQUE NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT NOT NULL,
+    category_id INT,
+    is_active BOOLEAN DEFAULT TRUE,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+""")
+
+conn.commit()
+conn.close()
+
+print("Database and tables created successfully!")
