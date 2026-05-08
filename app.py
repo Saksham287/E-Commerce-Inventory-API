4c6d66422bc553efddd7b73a8a49ca6aadd53d69
Saksham Gupta <saksham@sakshams-air.mynetworksettings.com>
2026-05-08 02:06:22 -0400

    Update

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+
 @app.route("/")
 def home():
-    return {"message": "API is running"}
+    return {"message": "E-Commerce Inventory API is running"}
 
 if __name__ == "__main__":
     app.run(debug=True)
