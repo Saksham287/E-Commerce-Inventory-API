57fefdc5e285bf15f54e4582fe24ae73fa7266bc
Saksham Gupta <saksham@sakshams-air.mynetworksettings.com>
2026-05-12 08:54:56 -0400

    global error handling

--- app.py
+++ app.py
@@ -23,5 +23,45 @@ app.register_blueprint(product_bp)
 def home():
     return {"message": "E-Commerce Inventory API is running"}
 
+@app.errorhandler(400)
+def bad_request(error):
+    return {
+        "status": "error",
+        "message": "Bad request"
+    }, 400
+
+
+@app.errorhandler(401)
+def unauthorized(error):
+    return {
+        "status": "error",
+        "message": "Unauthorized"
+    }, 401
+
+
+@app.errorhandler(403)
+def forbidden(error):
+    return {
+        "status": "error",
+        "message": "Forbidden"
+    }, 403
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return {
+        "status": "error",
+        "message": "Route not found"
+    }, 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return {
+        "status": "error",
+        "message": "Internal server error"
+    }, 500
+
+
 if __name__ == "__main__":
     app.run(debug=True)
